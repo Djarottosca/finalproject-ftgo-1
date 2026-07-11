f48073d4133ee543473b4c39b5523b415db146e2
Erdin <erdinhrmwn@gmail.com>
2026-07-11 08:42:40 +0800

    feat: add jwt manager and http response helpers

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,62 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JwtClaims struct {
+	jwt.RegisteredClaims
+	UserID uint   `json:"user_id"`
+	Role   string `json:"role"`
+}
+
+type AuthManager struct {
+	secret string
+}
+
+func NewAuthManager(secret string) *AuthManager {
+	return &AuthManager{
+		secret: secret,
+	}
+}
+
+func (am *AuthManager) GenerateToken(userID uint, role string) (*string, error) {
+	claims := JwtClaims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(am.secret))
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenString, nil
+}
+
+func (am *AuthManager) VerifyToken(tokenString string) (*JwtClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JwtClaims{}, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(am.secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*JwtClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
